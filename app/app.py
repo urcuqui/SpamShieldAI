cfe908bb01e6fbea954f31b056e0c84f9227bf59
Christian Urcuqui <ulcamilo@gmail.com>
2025-06-20 15:28:42 -0500

    updates

--- app/app.py
+++ app/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 import random
+import joblib
 
 app = Flask(__name__)
 
+import os
+model_filename = os.path.join(os.path.dirname(__file__), 'notebooks/spam_detection_model.joblib')
+#model_filename = 'notebooks/spam_detection_model.joblib'
+loaded_model = joblib.load(model_filename)
 # Simulación de un modelo de clasificación de spam
 def classify_message(text):
-    spam_keywords = ["win", "free", "money", "offer", "click"]
-    if any(word in text.lower() for word in spam_keywords):
+    #spam_keywords = ["win", "free", "money", "offer", "click"]
+    predictions = loaded_model.predict([text])
+    if predictions[0] == 1:
         return "Spam"
     else:
         return "Not Spam"
@@ -18,9 +24,11 @@ def index():
 @app.route("/classify", methods=["POST"])
 def classify():
     data = request.get_json()
-    message = data.get("message", "")
+    message = data.get("message", "")   
     result = classify_message(message)
     return jsonify({"result": result})
 
 if __name__ == "__main__":
+   
+    
     app.run(debug=True)
